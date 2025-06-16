28a1ac51116e5846ae51f2a54d46727987987ed7
Fayiz <fayiz.islam@gmail.com>
2025-06-17 02:58:14 +0600

    added stuff

--- src/components/Home.js
+++ src/components/Home.js
@@ -38,26 +38,26 @@ export function Home() {
       <ProjectDemo
         title="Ecommerce Website"
         videoUrl="https://www.youtube.com/embed/YOUR_WEATHER_APP_VIDEO_ID"
-        projectLink="https://essential-threads-frontend.vercel.app/"
+        projectLink="https://essentialthreads.netlify.app/"
         description="A fully functioning e-commerce website built with React JS and Django as a practice project. It includes PayPal and credit card payment integration and is fully mobile responsive."
       />
 
       <ProjectDemo
         title="Portfolio Website"
         videoUrl="https://www.youtube.com/embed/YOUR_PORTFOLIO_VIDEO_ID"
-        projectLink="https://portfolio-2he6kit4c-fayiz2005s-projects.vercel.app/"
+        projectLink="https://portfoliofayiz.netlify.app/"
         description="My personal portfolio built with React. Showcases my skills, projects, and background with a clean, responsive design."
       />
       <ProjectDemo
         title="Rock Paper Scissors game"
         videoUrl="https://www.youtube.com/embed/YOUR_PORTFOLIO_VIDEO_ID"
-        projectLink="https://rps-inky.vercel.app/"
+        projectLink="https://rpsgamefayiz.netlify.app/"
         description="This was the first project I ever made. It took a lot of effort beleive it or not"
       />
       <ProjectDemo
         title="Tic Tac Toe"
         videoUrl="https://www.youtube.com/embed/YOUR_PORTFOLIO_VIDEO_ID"
-        projectLink="https://tic-tac-toe-tau-ten-78.vercel.app/"
+        projectLink="https://tic-tac-toe-fayiz.netlify.app/"
         description="This is that one game you played in 5th grade. I honestly had nostalgia while making this. This is the one thing I miss about middle school."
       />
     </div>
